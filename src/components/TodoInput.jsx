7fa467baaab99c71e148d09fdc65f706f3858e55
Hang Dao <hangdao2003@gmail.com>
2024-10-18 13:12:59 +0700

    Fix addTodo and editTodo displaying wrong date content

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,20 +1,30 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 export default function TodoInput(props) {
     const { todoValue, setTodoValue, todoTime, setTodoTime, handleAddTodos } = props
+    const today = new Date()
+    const todayString = (
+        (today.getMonth() + 1 < 10) ? 
+        `${today.getFullYear()}-0${today.getMonth() + 1}-${today.getDate()}`: 
+        `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`
+    )
+    
+    useEffect(() => {
+        setTodoTime(todayString)
+    }, [])
 
     return (
         <header>
             <input value={todoValue} onChange={(e) => {
                 setTodoValue(e.target.value)
             }} placeholder="Enter todo..."/>
-            <input type="date" min="24-10-18" onChange={(e) => {
+            <input type="date" value={todoTime} min="24-10-18" onChange={(e) => {
                 setTodoTime(e.target.value)
             }}/>
             <button onClick={() => {
-                handleAddTodos({ content: todoValue, time: todoTime})
+                handleAddTodos({ content: todoValue, time: todoTime })
                 setTodoValue('')
-                setTodoTime('')
+                setTodoTime(todayString)
             }}>Add</button>
         </header>
     )
